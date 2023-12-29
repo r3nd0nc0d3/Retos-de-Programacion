d75e0d131b347031899e3802221764c9dddd3dcd
r3nd0nc0d3 <105399428+r3nd0nc0d3@users.noreply.github.com>
2023-12-29 08:49:47 -0600

    reto #1

---
+++ 01-reto.js
@@ -0,0 +1,13 @@
+// https://www.javascript.com/
+/*
+https://www.javascript.com/
+*/
+
+let miName = "Mateo Rendon";
+const birthYear = 1993;
+
+let language = "JavaScript";
+let year = 2024;
+let learn = true;
+
+console.log("Hola, " + language + "!");
