5ed011f7983877b748d39f360fed1b1a20ea8e94
r3nd0nc0d3 <105399428+r3nd0nc0d3@users.noreply.github.com>
2023-12-29 09:04:31 -0600

    Add files via upload

--- 01-reto.js
+++ 01-reto.js
@@ -3,11 +3,32 @@
 https://www.javascript.com/
 */
 
+// CREATE A VARTIABLE
 let miName = "Mateo Rendon";
 const birthYear = 1993;
 
+// TYPES OF PRIMITIVES
 let language = "JavaScript";
 let year = 2024;
 let learn = true;
+
+typeof language;
+typeof year;
+typeof learn;
+typeof null;
+typeof undefined;
+typeof Symbol;
+typeof 1n;
+
+// CONSOLE LOGS
+/*
+console.log(typeof language);
+console.log(typeof year);
+console.log(typeof learn);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Symbol);
+console.log(typeof 1n);
+*/
 
 console.log("Hola, " + language + "!");
